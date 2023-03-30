ca9ca798c7dcc96c26aec32d835600133d0e7ba8
no0b2no0b <121117941+no0b2no0b@users.noreply.github.com>
2023-03-30 13:03:51 +0530

    footer and nav update

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
 import './footer.css'
+import {FaFacebookF} from 'react-icons/fa'
+import {FiInstagram} from 'react-icons/fi'
+import {IoLogoTwitter} from 'react-icons/io'
 
 const Footer = () => {
   return (
-    <section>
-      Footer
-    </section>
+    <footer>
+      <a href="#" className='footer__logo'>Noob</a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experiences">Experience</a></li>
+        <li><a href="#services">service</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://facebook.com"><FaFacebookF/></a>
+        <a href="https://instagram.com"><FiInstagram/></a>
+        <a href="https://twitter.com"><IoLogoTwitter/></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; NOOB. All rights reserved. </small>
+      </div>
+    </footer>
   )
 }
 
